1578fdafc868b161c80e2823223ab27c706f6457
Trevor Stanhope <trevor@W510>
2014-02-20 08:54:41 -0500

    small changes

--- HiveMind_Server.py
+++ HiveMind_Server.py
@@ -12,17 +12,19 @@ from firebase import firebase, jsonutil
 app = Flask(__name__)
 base = firebase.FirebaseApplication('https://hivemind-plus.firebaseio.com', None)
 
-# Flask Functions
+# Index
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# User
 @app.route('/<username>')
 def user(username):
     return render_template('user.html',
         username=username
     )
 
+# Aggregator
 @app.route('/<username>/<aggregator>')
 def aggregator(username, aggregator):
     return render_template('aggregator.html',
@@ -30,6 +32,7 @@ def aggregator(username, aggregator):
 	    username=username
     )
 
+# Graph
 @app.route('/<username>/<aggregator>/<graph>')
 def graph(username, aggregator, graph):
     return render_template('graph.html',
@@ -38,6 +41,7 @@ def graph(username, aggregator, graph):
         graph=graph
     )
 
+# 404
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
