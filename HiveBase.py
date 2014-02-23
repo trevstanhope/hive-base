d69294bd93b01c4a111d2ea6f81804d87e7de693
Trevor Stanhope <trevor@W510>
2014-02-23 09:47:17 -0500

    changes during mchacks

--- HiveBase.py
+++ HiveBase.py
@@ -5,28 +5,25 @@ Developed by Trevor Stanhope
 """
 
 # Libraries
-from flask import Flask, url_for, render_template, request
+from flask import Flask, url_for, render_template, request, redirect, session
 from firebase import firebase, jsonutil
 from flask_oauth import OAuth
-from flask import redirect
-from flask import session
 
-# OAuth
+# Global Objects
+app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+base = firebase.FirebaseApplication('https://hivemind-plus.firebaseio.com', None)
 oauth = OAuth()
 twitter = oauth.remote_app('twitter',
     base_url='https://api.twitter.com/1/',
     request_token_url='https://api.twitter.com/oauth/request_token',
     access_token_url='https://api.twitter.com/oauth/access_token',
     authorize_url='https://api.twitter.com/oauth/authorize',
-    consumer_key='NQq1xm5OoL84mPAIoy3iFw',
-    consumer_secret='7d3ED8qT3I40PTEP4uyLyGhecnqD56w579FmSnSY'
+    consumer_key='7x3M6ZT9dbO6pr1zeiutg',
+    consumer_secret='534Mkp7RJZ5asaC9iURV6hGa89gQhQXUDuPo9Ing'
 )
 
-# Global Objects
-app = Flask(__name__)
-base = firebase.FirebaseApplication('https://hivemind-plus.firebaseio.com', None)
-
-# Twitter Getter
+# Getter
 @twitter.tokengetter
 def get_twitter_token(token=None):
     return session.get('twitter_token')
@@ -35,25 +32,22 @@ def get_twitter_token(token=None):
 @app.route('/login')
 def login():
     return twitter.authorize(callback=url_for('oauth_authorized',
-        next=request.args.get('next') or request.referrer or None))
+        next=(request.args.get('next') or request.referrer or None)))
 
-# Authorized
-@app.route('/oauth-authorized')
+# OAuth
+@app.route('/oauth_authorized')
 @twitter.authorized_handler
 def oauth_authorized(resp):
     next_url = request.args.get('next') or url_for('index')
     if resp is None:
-        flash(u'You denied the request to sign in.')
         return redirect(next_url)
-
-    session['twitter_token'] = (
-        resp['oauth_token'],
-        resp['oauth_token_secret']
-    )
-    session['twitter_user'] = resp['screen_name']
-
-    flash('You were signed in as %s' % resp['screen_name'])
-    return redirect(next_url)
+    else:
+        session['twitter_token'] = (
+            resp['oauth_token'],
+            resp['oauth_token_secret']
+        )
+        session['twitter_user'] = resp['screen_name']
+        return redirect(next_url)
 
 # Index
 @app.route('/')
