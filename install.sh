dc23f94bba7951d4e6e2786df299ea6fe257e4ae
Trevor Stanhope <trevor@W510>
2014-02-24 16:52:47 -0500

    jenkins, etc

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+wget -q -O http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
 apt-get update
 apt-get upgrade
-apt-get install pip
+apt-get install pip virtualenv curl
 pip install -r requirements.txt
+
+
 
