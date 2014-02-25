20b88ebd6a4bc019188c0dfd429007106caf2dd6
Trevor Stanhope <trevor@W510>
2014-02-25 17:37:55 -0500

    missing bracket

--- HiveFeed.py
+++ HiveFeed.py
@@ -54,7 +54,7 @@ def oauth_authorized(resp):
         )
         session['twitter_user'] = resp['screen_name']
         for field in session:
-            print('--> ' + session[field])
+            print('--> ' + str(ession[field]))
         return redirect(url_for(session['twitter_user']))
 
 # Index
