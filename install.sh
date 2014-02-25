dbce2ec180f791285a70a1d766b3c31dfe152134
Trevor Stanhope <trevor@W510>
2014-02-25 11:25:06 -0500

    includes flask

--- install.sh
+++ install.sh
@@ -7,9 +7,6 @@ pip install -r requirements.txt
 cp apache/* /etc/apache2/sites-available/
 a2enmod proxy
 a2ensite jenkins
-<<<<<<< HEAD
 a2ensite flask
-=======
->>>>>>> e833faead1c0ceea92c91566a7cf41a72b5b37b0
 a2dissite default
 
