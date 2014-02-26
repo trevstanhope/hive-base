daf6972c0509c9e56363be38783cded16bdb2ff6
Trevor Stanhope <trevor@W510>
2014-02-26 08:59:19 -0500

    auto delete twitter tokens

--- HiveFeed.py
+++ HiveFeed.py
@@ -38,6 +38,8 @@ twitter = oauth.remote_app('twitter',
 # Twitter Session
 @twitter.tokengetter
 def get_twitter_token(token=None):
+    if session.has_key('twitter_tokens'):
+        del session['twitter_tokens']
     return session.get('twitter_token')
 
 # Index
