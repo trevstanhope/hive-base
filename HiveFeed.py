ae402698f8da4405c2575eeb6361d485b4dfd614
Trevor Stanhope <trevor@W510>
2014-02-26 15:42:57 -0500

    chang

--- HiveFeed.py
+++ HiveFeed.py
@@ -76,7 +76,7 @@ def oauth_authorized(resp):
             resp['oauth_token_secret']
         )
         session['twitter_user'] = resp['screen_name']
-        return redirect(url_for('user', next=session['twitter_user']))
+        return redirect('/user/' + session['twitter_user'])
 
 # User
 @app.route('/user/<username>')
@@ -102,7 +102,7 @@ def tweet(log):
         print(str(resp.status) + ': Resource not found.')
     else:
         print(str(resp.status) + ': Other')
-    return redirect(url_for('user', next=session['twitter_user']))
+    return redirect('/user/' + session['twitter_user'])
 
 # Aggregator
 @app.route('/user/<username>/<aggregator>')
